7860d4955ffbc83e12fea0f676e8ebe240539d1a
TesEzzo <francesco.barranca00@gmail.com>
2023-11-10 13:32:50 +0100

    esercizi destrutturazione finiti

---
+++ javascript_exercise/compiti/ex11.js
@@ -0,0 +1,5 @@
+const colors = ["red", "green", "blue"];
+
+const [firstColor, secondColor, ThirdColor] = colors;
+
+console.log(`The pixel colors are: ${firstColor}, ${secondColor} and ${ThirdColor}.`);
