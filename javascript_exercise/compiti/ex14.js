47a32c6cb549feb7234ff7995c88fecd8f12f596
TesEzzo <francesco.barranca00@gmail.com>
2023-11-13 11:07:12 +0100

    esercizi corretti... forse

--- javascript_exercise/compiti/ex14.js
+++ javascript_exercise/compiti/ex14.js
@@ -1,5 +1,7 @@
 function filterOutOdds(...numbers) {
-    console.log(numbers.filter(numbers=> numbers % 2 === 0));
+    return(numbers.filter(numbers=> numbers % 2 === 0));
 };
 
-filterOutOdds(1,2,3,4,5,6,7,8,9,10,11,12);
+const pari=filterOutOdds(1,2,3,4,5,6,7,8,9,10,11,12);
+
+console.log(pari);
--- javascript_exercise/compiti/ex6.js
+++ javascript_exercise/compiti/ex6.js
@@ -1,4 +1,3 @@
 const num1 = 10;
 const num2 = 20;
-const sum = num1 + num2
-console.log(`${sum}`);
+console.log(`${num1 + num2}`);
