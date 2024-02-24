c37b07454a974340cc7ec2f296185759d800ee44
TesEzzo <francesco.barranca00@gmail.com>
2024-02-24 17:07:06 +0100

    ex10

---
+++ node/ex10.js
@@ -0,0 +1,27 @@
+const luckyDraw = (player) => {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+};
+
+const executeDrow = () => {
+    const players = ["Tina", "Jorge", "Julien"];
+    players.forEach(async (player) => {
+        try {
+          const result = await luckyDraw(player);
+          console.log(result);
+        } catch (error) {
+          console.log(error.message);
+        }
+      });
+}
+
+executeDrow();
